9f534eca4f9132756ce3c5abf538ba51f2d6350a
Sachin Jaiswal <sachin.jaiswal@techdome.net.in>
2024-05-08 01:58:40 +0530

    question right

--- program1.js
+++ program1.js
@@ -1,9 +1,34 @@
   const getTotalIsles = function (grid) {
-
-
-    // write your code here
-
-  };
+    let count = 0;
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[0].length; j++) {
+        if (grid[i][j] === 'L') {
+          count++;
+          color(grid, i, j); 
+        }   
+      }   
+    }
+    return count;
+  }
+   
+  function color(grid, i, j) {
+    let q = [[i, j]];
+    while (q.length) {
+      let [x, y] = q.pop();
+      grid[x][y] = 0;
+      for (let [newX, newY] of [[x + 1, y], [x, y + 1], [x - 1, y], [x, y - 1]]) {
+        if (
+          newX >= 0 &&
+          newX < grid.length &&
+          newY >= 0 &&
+          newY < grid[0].length &&
+          grid[newX][newY] === 'L' 
+        ) { 
+          q.push([newX, newY]);
+        }   
+      }   
+    }
+  }
   
   module.exports = getTotalIsles;
 
